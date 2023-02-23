2abfcab3834fc0a3b281cf5297aec388b317c40a
ignaciozhetta <zagu4988@gmail.com>
2023-02-23 12:52:35 -0300

    [feat] Initial configurations

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express  from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+class ServerBootstrap {
+    public app: express.Application = express();
+    private port: number = 8000;
+
+    constructor () {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended:true}));
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.listen();
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log("Server listening on port: " + this.port);
+        });
+    }
+}
+
+new ServerBootstrap();
