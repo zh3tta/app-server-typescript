365e0bff212e2aa58de5c025f1cf090c507cd46f
ignaciozhetta <zagu4988@gmail.com>
2023-02-27 11:41:10 -0300

    [chore] Folder architecture added

--- src/controllers/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export class UserController {
-    getUsers(req: Request, res: Response) {
-        res.status(200).json({
-            user: "byZhetta",
-        });
-    }
-}
