7f8b97627862b28ee9b13baebd58188b671bc53c
ignaciozhetta <zagu4988@gmail.com>
2023-02-27 13:07:52 -0300

    [chore] Folder architecture added

---
+++ src/user/user.router.ts
@@ -0,0 +1,12 @@
+import { BaseRouter } from "../shared/router/router";
+import { UserController } from "./controllers/user.controller";
+
+export class UserRouter extends BaseRouter<UserController> {
+    constructor() {
+        super(UserController);
+    }
+
+    routes(): void {
+        this.router.get("/user", (req, res) => this.controller.getUsers(req, res));
+    }
+}
