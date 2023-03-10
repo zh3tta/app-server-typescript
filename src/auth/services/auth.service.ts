d366962abcab850e9d850c31c9011116b247d856
ignaciozhetta <zagu4988@gmail.com>
2023-03-10 12:24:44 -0300

    [feat] Hashing, auth and query builders

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,57 @@
+import { ConfigServer } from "../../config/config";
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+import { UserService } from "../../user/services/user.service";
+import { UserEntity } from "../../user/entities/user.entity";
+import { PayloadToken } from "../interface/auth.interface";
+
+export class AuthService extends ConfigServer {
+    constructor(
+        private readonly userService: UserService = new UserService(),
+        private readonly jwtInstance = jwt
+    ) {
+        super();
+    }
+
+    public async validateUser(username: string, password: string): Promise<UserEntity | null> {
+        const userByEmail = await this.userService.findUserByEmail(username);
+        const userByUsername = await this.userService.findUserByUsername(username);
+
+        if (userByUsername) {
+            const isMatch = await bcrypt.compare(password, userByUsername.password);
+            isMatch && userByUsername;
+        }
+
+        if (userByEmail) {
+            const isMatch = await bcrypt.compare(password, userByEmail.password);
+            isMatch && userByEmail;
+        }
+
+        return null;
+    }
+
+    sing(payload: jwt.JwtPayload, secret: any) {
+        return this.jwtInstance.sign(payload, secret);
+    }
+
+    public async generateJWT(user: UserEntity): Promise<{ accessToken: string; user: UserEntity }> {
+        const userConsult = await this.userService.findUserWithRole(
+            user.id,
+            user.role
+        );
+
+        const payload: PayloadToken = {
+            role: userConsult!.role,
+            sub: userConsult!.id,
+        };
+
+        if (userConsult) {
+            user.password = "Not permission";
+        }
+
+        return {
+            accessToken: this.sing(payload, this.getEnvironment("JWT_SECRET")),
+            user,
+        };
+    }
+}
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { DeleteResult, UpdateResult } from "typeorm";
+import * as bcrypt from "bcrypt";
 import { BaseService } from "../../config/base.service";
-import { UserDTO } from "../dto/user.dto";
+import { RoleType, UserDTO } from "../dto/user.dto";
 import { UserEntity } from "../entities/user.entity";
 
 export class UserService extends BaseService<UserEntity> {
@@ -16,16 +17,45 @@ export class UserService extends BaseService<UserEntity> {
         return (await this.execRepository).findOneBy({ id });
     }
 
+    async findUserWithRole(id: string, role: RoleType): Promise<UserEntity | null> {
+        const user = (await this.execRepository)
+            .createQueryBuilder("user")
+            .where({ id })
+            .andWhere({ role })
+            .getOne();
+
+        return user;
+    }
+    
     async findUserWithRelation(id: string): Promise<UserEntity | null> {
         return (await this.execRepository)
-        .createQueryBuilder("user")
-        .leftJoinAndSelect("user.customer", "customer")
-        .where({ id })
-        .getOne();
+            .createQueryBuilder("user")
+            .leftJoinAndSelect("user.customer", "customer")
+            .where({ id })
+            .getOne();
     }       
     
+    async findUserByEmail(email: string): Promise<UserEntity | null> {
+        return (await this.execRepository)
+            .createQueryBuilder("user")
+            .addSelect("user.password")
+            .where({ email })
+            .getOne();
+    }
+
+    async findUserByUsername(username: string): Promise<UserEntity | null> {
+        return (await this.execRepository)
+            .createQueryBuilder("user")
+            .addSelect("user.password")
+            .where({ username })
+            .getOne();
+    }
+    
     async createUser(body: UserDTO): Promise<UserEntity> {
-        return (await this.execRepository).save(body);
+        const newUser = (await this.execRepository).create(body);
+        const hash = await bcrypt.hash(newUser.password, 10);
+        newUser.password = hash;
+        return (await this.execRepository).save(newUser);
     }
 
     async deleteUser(id: string): Promise<DeleteResult> {
